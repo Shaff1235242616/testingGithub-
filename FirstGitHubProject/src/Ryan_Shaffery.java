5e0025a055d0e2e0accb3aa1e26dc58660dde7bc
Anya265 <113116234+Anya265@users.noreply.github.com>
2024-02-21 13:08:36 -0500

    hi shaff

--- FirstGitHubProject/src/Ryan_Shaffery.java
+++ FirstGitHubProject/src/Ryan_Shaffery.java
@@ -3,5 +3,8 @@ public class Ryan_Shaffery {
         System.out.println("hi shaffery");
         System.out.println("Hi Shaff");
         System.out.println("Hello other editor");
+        System.out.println("Hi Shaff");
+        System.out.println("Hi Nick");
+        System.out.println("Hi Elijah");
     }
 }
